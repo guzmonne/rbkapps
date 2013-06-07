6e0b8b30daf9e9bdaa36fd43a7909decbfc72fcb
Guzman Monne <guzmonne@hotmail.com>
2013-06-07 12:29:20 -0300

    hacked email version

--- app/controllers/service_requests_controller.rb
+++ app/controllers/service_requests_controller.rb
@@ -10,7 +10,15 @@ class ServiceRequestsController < ApplicationController
   end
 
   def create
-    respond_with ServiceRequest.create(params["service_request"])
+    @service_request = ServiceRequest.new(params["service_request"])
+    respond_to do |format|
+      if @service_request.save
+        # Despues de que graba
+        ServiceRequestMailer.service_request_created_email(User.find(@service_request.creator_id), @service_request)
+
+        format.json { render :json => @service_request, :status => :created, :location => @service_request }
+      end
+    end
   end
 
   def update
